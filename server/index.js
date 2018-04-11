8f43e929a2481382f9d54897535c79e5c0e56d20
Marcello Messori <mmessori@castgroup.it>
2018-04-11 12:29:47 +0200

    [ADD] local jQuery, in order to work offline

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const app = require('express')()
-      http = require('http').Server(app)
-      io = require('socket.io')(http)
-      path = require('path')
-      PORT = 3000;
+const express = require('express')
+    , app = express()
+    , http = require('http').Server(app)
+    , io = require('socket.io')(http)
+    , path = require('path')
+    , PORT = 3000;
 
 app.get('/', (req, res) => {
    res.sendFile(`${path.dirname(__dirname)}/client/index.html`);
 });
+
+app.use('/public', express.static(`${path.dirname(__dirname)}/client/public`));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
